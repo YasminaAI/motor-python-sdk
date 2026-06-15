791b2f46cf43714ab5abeb26efab7872c85529b4
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2026-06-15 14:00:55 +0000

    [fern-generated] Update SDK
    Generated by Fern
    CLI Version: unknown
    Generators:
      - fernapi/fern-python-sdk: 5.14.15

--- src/yasminaai/core/client_wrapper.py
+++ src/yasminaai/core/client_wrapper.py
@@ -29,12 +29,12 @@ class BaseClientWrapper:
         import platform
 
         headers: typing.Dict[str, str] = {
-            "User-Agent": "yasmina-motor-python/0.0.1",
+            "User-Agent": "motor-python-sdk/0.0.2",
             "X-Fern-Language": "Python",
             "X-Fern-Runtime": f"python/{platform.python_version()}",
             "X-Fern-Platform": f"{platform.system().lower()}/{platform.release()}",
-            "X-Fern-SDK-Name": "yasmina-motor-python",
-            "X-Fern-SDK-Version": "0.0.1",
+            "X-Fern-SDK-Name": "motor-python-sdk",
+            "X-Fern-SDK-Version": "0.0.2",
             **(self.get_custom_headers() or {}),
         }
         headers["Authorization"] = f"Bearer {self._get_token()}"
